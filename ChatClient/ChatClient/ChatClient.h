9b04b7dc3982fae9663f5c9edea5747cfbaef1d0
KYHyeon <run190822@gmail.com>
2023-11-12 14:24:09 +0900

    ChatClient 프로젝트 생성

---
+++ ChatClient/ChatClient/ChatClient.h
@@ -0,0 +1,18 @@
+//
+//  ChatClient.h
+//  ChatClient
+//
+//  Created by 현기엽 on 2023/11/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatClient.
+FOUNDATION_EXPORT double ChatClientVersionNumber;
+
+//! Project version string for ChatClient.
+FOUNDATION_EXPORT const unsigned char ChatClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatClient/PublicHeader.h>
+
+
---
+++ ChatClient/ChatClient/ChatClient.swift
@@ -0,0 +1,11 @@
+//
+//  ChatClient.swift
+//  ChatClient
+//
+//  Created by 현기엽 on 2023/11/12.
+//
+
+import Foundation
+
+public final class ChatClient {
+}
